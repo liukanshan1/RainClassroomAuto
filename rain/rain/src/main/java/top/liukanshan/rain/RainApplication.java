f82e5cb97917aedb7f522d8da20589a50ae9dd65
Aroc <85719360+liukanshan1@users.noreply.github.com>
2023-10-09 11:49:05 +0800

    spb

---
+++ rain/rain/src/main/java/top/liukanshan/rain/RainApplication.java
@@ -0,0 +1,13 @@
+package top.liukanshan.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainApplication.class, args);
+    }
+
+}
---
+++ rain/rain/src/test/java/top/liukanshan/rain/RainApplicationTests.java
@@ -0,0 +1,13 @@
+package top.liukanshan.rain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
